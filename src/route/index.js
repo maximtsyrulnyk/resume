152a1e5c4b2b55a61a7b228ad0904184137f1a6a
maximtsyrulnyk <maximtsyrulnyk@gmail.com>
2023-05-08 23:28:56 +0300

    upd packages

--- src/route/index.js
+++ src/route/index.js
@@ -64,25 +64,17 @@ router.get('/summary', function (req, res) {
           tournament position, goals etc), analyzing by simple mathematics models and preparing probability
           for such events like: money line - first win / draw / second win, totals etc.`,
       },
-      skills: [
-        'HTML',
-        'Handlebars',
-        'VS Code',
-        'Git',
-        'Terminal',
-        'NPM',
-      ],
     },
     footer,
   })
 })
 
 // ================================================
-
+/*
 router.get('/', function (req, res) {
   res.render('index', {})
 })
-
+*/
 router.get('/skills', function (req, res) {
   res.render('skills', {
     page: { title: 'Resume | Skills' },
@@ -93,6 +85,19 @@ router.get('/skills', function (req, res) {
         title: 'Summary',
         text: `Open-minded for new technologies, with 1 year of experience in development. Whenever`,
       },
+      skills: [
+        { name: 'HTML', point: false, isTop: false },
+        { name: 'Handlebars', point: 4, isTop: true },
+        { name: 'VS Code', point: 10, isTop: false },
+        { name: 'Git', point: 10, isTop: true },
+        { name: 'Terminal', point: 2, isTop: false },
+        { name: 'NPM', point: 6, isTop: true },
+      ],
+      hobbies: [
+        { name: 'Football', isFavourite: false },
+        { name: 'PC games', point: 4, isFavourite: true },
+        { name: 'Books', point: 10, isFavourite: false },
+      ],
     },
     footer,
   })
@@ -102,14 +107,16 @@ router.get('/education', function (req, res) {
     page: { title: 'Resume | Education' },
     header,
     main: {
-      skills: [
-        { name: 'HTML', point: 10, isTop: true },
-        { name: 'Handlebars', point: 10, isTop: true },
-        { name: 'VS Code & NPM', point: 9, isTop: false },
-        { name: 'Git & Terminal', point: 7 },
-        { name: 'React.js', point: 0 },
-        { name: 'PHP', point: null },
+      educations: [
+        { name: 'HTML', isEnd: true },
+        { name: 'Handlebars', isEnd: false },
+        { name: 'VS Code', isEnd: true },
+        { name: 'Git', isEnd: false },
+        { name: 'Terminal', isEnd: false },
+        { name: 'NPM', isEnd: false },
       ],
+
+      certificates: [{ name: 'HTML' }, { name: 'VS Code' }],
     },
 
     footer,
@@ -146,6 +153,7 @@ router.get('/work', function (req, res) {
                 { name: 'HTML / CSS' },
                 { name: 'Nodejs' },
               ],
+
               awards: [
                 {
                   name: 'Background  verification - is a feature that provides users to prove that they are real persons.',
@@ -154,8 +162,8 @@ router.get('/work', function (req, res) {
                   name: 'Preparing SEO optimized pages. The automated ',
                 },
               ],
-              stackAmaunt: 3,
-              awardAmaunt: 2,
+              stackAmount: 3,
+              awardAmount: 2,
             },
           ],
         },
@@ -163,6 +171,197 @@ router.get('/work', function (req, res) {
     },
 
     footer,
+  })
+})
+
+router.get('/person', function (req, res) {
+  res.render('person', {
+    layout: 'person',
+    person: {
+      name: 'Emma Johnson',
+      age: 32,
+      gender: 'Female',
+      address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        zip: '10001',
+        country: 'USA',
+      },
+      education: [
+        {
+          degree: 'Bachelor of Science',
+          major: 'Computer Science',
+          university:
+            'Massachusetts Institute of Technology',
+          graduationYear: 2012,
+        },
+      ],
+      workExperience: [
+        {
+          company: 'Google',
+          title: 'Software Engineer',
+          startDate: '2012-06-01',
+          endDate: '2016-12-31',
+          responsibilities: [
+            'Developed new features for Google Maps',
+            'Worked on improving search algorithms',
+          ],
+          year_founded: 1990,
+          industry: 'Technology',
+          employees: [
+            {
+              name: 'John Smith',
+              position: 'CEO',
+              department: 'Executive',
+              projects: [
+                {
+                  name: 'Project Alpha',
+                  description:
+                    'Developing new software platform',
+                  status: 'In Progress',
+                  teams: [
+                    {
+                      team_name: 'Awesome Team',
+                      team_leader: {
+                        name: 'John Smath',
+                        title: 'Team Leader',
+                        email: 'john.smith@example.com',
+                      },
+                      team_members: [
+                        {
+                          name: 'Alice Johnson',
+                          title: 'Software Engineer',
+                          email:
+                            'alice.johnson@example.com',
+                          skills: ['Java', 'Python', 'SQL'],
+                          projects: [
+                            {
+                              name: 'Project A',
+                              description:
+                                'Lorem ipsum dolor sit amet',
+                              technologies: [
+                                'Java',
+                                'Spring Framework',
+                              ],
+                              team_members: [
+                                {
+                                  name: 'Bob Lee',
+                                  title:
+                                    'Software Engineer',
+                                },
+                                {
+                                  name: 'Cindy Chen',
+                                  title: 'UI Designer',
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  })
+})
+
+router.get('/bio', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('bio', {
+    name: 'Albert Einstein',
+    birthdate: 'March 14, 1879',
+    birthplace:
+      'Ulm, Kingdom of Württemberg, German Empire',
+    deathdate: 'April 18, 1955',
+    deathplace: 'Princeton, New Jersey, United States',
+    nationality: 'Swiss, German, American',
+    occupation: 'Theoretical Physicist',
+    known_for: [
+      'Theory of Relativity',
+      'E=mc²',
+      'Photoelectric Effect',
+      'Brownian Motion',
+    ],
+    education: [
+      {
+        degree: 'Doctor of Philosophy',
+        field: 'Physics',
+        institution: 'University of Zurich',
+        year: 1905,
+      },
+    ],
+    notable_publications: [
+      {
+        title: 'On the Electrodynamics of Moving Bodies',
+        year: 1905,
+        publisher: 'Annalen der Physik',
+      },
+      {
+        title:
+          'Does the Inertia of a Body Depend Upon Its Energy Content?',
+        year: 1905,
+        publisher: 'Annalen der Physik',
+      },
+    ],
+    partners: [
+      {
+        name: 'Mileva Marić',
+        relationship: 'First wife',
+        years: '1903-1919',
+      },
+      {
+        name: 'Elsa Einstein',
+        relationship:
+          "Second wife, also Einstein's first cousin",
+        years: '1919-1936',
+      },
+    ],
+    awards: [
+      {
+        title: 'Nobel Prize in Physics',
+        year: 1921,
+        description:
+          'Awarded for his explanation of the photoelectric effect',
+      },
+    ],
+    influences: [
+      'Isaac Newton',
+      'James Clerk Maxwell',
+      'Hermann Minkowski',
+    ],
+    influenced: [
+      'Niels Bohr',
+      'Erwin Schrödinger',
+      'Werner Heisenberg',
+      'Richard Feynman',
+    ],
+    quotes: [
+      'Imagination is more important than knowledge.',
+      'I have no special talent. I am only passionately curious.',
+      'The important thing is not to stop questioning.',
+      'The most beautiful thing we can experience is the mysterious. It is the source of all true art and all science.',
+    ],
+    major_discoveries: [
+      {
+        title: 'Photoelectric Effect',
+        year: 1905,
+        description:
+          'Einstein explained the photoelectric effect, where electrons are emitted from a metal surface when it is illuminated by light.',
+      },
+    ],
+    contributions: {
+      title: 'Inventions',
+      description:
+        'Leonardo designed and invented a wide variety of machines and devices, including a helicopter, a diving suit, and a self-propelled cart. Many of his inventions were centuries ahead of their time.',
+      year: 'Late 15th to early 16th century',
+      field: 'Invention',
+    },
   })
 })
 
